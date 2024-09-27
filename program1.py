9ee99561c34dc38f4a8c363fd9c324e10d9bef02
kumkumsahu1505 <kumkumsahu1505@gmail.com>
2024-09-27 12:46:15 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,32 +1,25 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-     
-     def count_islands(grid):
-         if not grid:
-           return 0
+        if not grid:
+            return 0
 
-    rows, cols = len(grid), len(grid[0])
-    visited = set()
-    islands = 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
 
-    def dfs(i, j):
-        if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 'W' or (i, j) in visited:
-            return
-        visited.add((i, j))
-        dfs(i + 1, j)
-        dfs(i - 1, j)
-        dfs(i, j + 1)
-        dfs(i, j - 1)
+        def dfs(i, j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 'W' or (i, j) in visited:
+                return
+            visited.add((i, j))
+            dfs(i + 1, j)  # down
+            dfs(i - 1, j)  # up
+            dfs(i, j + 1)  # right
+            dfs(i, j - 1)  # left
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 'L' and (i, j) not in visited:
-                islands += 1
-                dfs(i, j)
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 'L' and (i, j) not in visited:
+                    islands += 1
+                    dfs(i, j)
 
-     return islands
-
-
-     return 0
+        return islands
