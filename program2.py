6ecf7bbeeddd7fb67a1cb545dfde5fa5a2f7a1f8
kumkumsahu1505 <kumkumsahu1505@gmail.com>
2024-09-27 12:40:40 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,33 +1,30 @@
-def num_islands(grid):
-    if not grid or len(grid) == 0:
+def decode_message( s: str, p: str) -> bool:
+
+# write your code here
+  
+      def count_islands(grid):
+    if not grid:
         return 0
 
     rows, cols = len(grid), len(grid[0])
-    visited = set()  
+    visited = set()
     islands = 0
 
-
-    def dfs(r, c):
-   
-        if (r < 0 or r >= rows or c < 0 or c >= cols or
-                grid[r][c] == 'W' or (r, c) in visited):
+    def dfs(i, j):
+        if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 'W' or (i, j) in visited:
             return
-        
-        # Mark the cell as visited
-        visited.add((r, c))
-        
-        # Visit all neighboring cells (up, down, left, right)
-        dfs(r + 1, c)  # down
-        dfs(r - 1, c)  # up
-        dfs(r, c + 1)  # right
-        dfs(r, c - 1)  # left
+        visited.add((i, j))
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
 
-    # Loop through each cell in the grid
-    for r in range(rows):
-        for c in range(cols):
-           
-            if grid[r][c] == 'L' and (r, c) not in visited:
-                islands += 1  # Found a new island
-                dfs(r, c)  # Mark all land cells connected to this one
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 'L' and (i, j) not in visited:
+                islands += 1
+                dfs(i, j)
 
     return islands
+
+        return False
