601aa9d5e21f55e773b884070d72bdd5239d15b6
kumkumsahu1505 <kumkumsahu1505@gmail.com>
2024-09-27 12:36:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,28 +2,5 @@ def decode_message( s: str, p: str) -> bool:
 
 # write your code here
   
-  def count_islands(grid):
-    if not grid:
-        return 0
-
-    rows, cols = len(grid), len(grid[0])
-    visited = set()
-    islands = 0
-
-    def dfs(i, j):
-        if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 'W' or (i, j) in visited:
-            return
-        visited.add((i, j))
-        dfs(i + 1, j)
-        dfs(i - 1, j)
-        dfs(i, j + 1)
-        dfs(i, j - 1)
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 'L' and (i, j) not in visited:
-                islands += 1
-                dfs(i, j)
-
-    return islands
+  
         return False
